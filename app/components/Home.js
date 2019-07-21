c92db0db5e26c6ceac29bac4accb3a87904ddc02
Inseo Baek <yunibaek@hotmail.com>
2019-07-21 15:04:17 +0200

    added basic http server open/close functionalities

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,15 +2,33 @@
 import React, { Component } from 'react';
 import styles from './Home.css';
 
+import {open, close} from '../../proxy';
+
 type Props = {};
 
 export default class Home extends Component<Props> {
   props: Props;
 
+  onClick = () => {
+    console.log('proxy open');
+    open();
+  };
+
+  onClose = () => {
+    console.log('proxy close');
+    close()
+  };
+
   render() {
     return (
       <div className={styles.container}>
         <p>O Proxy</p>
+        <button type="button" onClick={this.onClick}>
+          Click me
+        </button>
+        <button type="button" onClick={this.onClose}>
+         CLOSE
+      </button>
       </div>
     );
   }
---
+++ proxy/index.js
@@ -0,0 +1,117 @@
+const url = require('url');
+const net = require('net');
+const http = require('http');
+
+function OProxy() {
+  const httpServer = http.createServer((req, res) => {
+    // Non proxy request
+    if (req.url.indexOf('http://') !== 0) {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      console.log(req.method, req.url);
+      console.log();
+
+      res.end(`O proxy :D`);
+      return;
+    }
+
+    // Straight http proxy
+    console.warn(`${req.method}(${req.url})`);
+    // Build new request to send to target
+    const outbound = http.request(
+      req.url,
+      {
+        headers: req.headers,
+        method: req.method
+      },
+      resp => {
+        // Headers target => client
+        res.writeHead(resp.statusCode, resp.headers);
+        // Body target => client
+        resp.pipe(res);
+        // When finished piping body, end request to client
+        resp.on('end', () => res.end());
+      }
+    );
+    // Body client => target
+    req.pipe(outbound);
+    // Handle errors
+    outbound.on('error', err => {
+      console.error(`PROXY_ERR(${req.url}): ${err}`);
+      res.writeHead(500);
+      res.end(`Unknown Error:${err}`);
+    });
+  });
+
+  // For proxied TCP & HTTPS connections
+  httpServer.on('connect', (req, socket, head) => {
+    const target = url.parse(`any://${req.url}`);
+    const { port, hostname } = target;
+    console.warn('target:', req.url);
+
+    // Cleanup closes sockets
+    const cleanup = () => {
+      try {
+        targetSocket.end();
+        socket.end();
+      } catch (err) {
+        console.error('err', err);
+      }
+    };
+
+    // onError prints errror then cleansup
+    const onError = err => {
+      console.error(`OProxy(${req.ip} => ${req.url}):`, err);
+      console.error();
+      cleanup();
+    };
+
+    const targetSocket = net.connect(Number(port), hostname, () => {
+      console.warn(`CONNECTED to ${hostname}:${port}`);
+
+      // Acknowledge that connection is established
+      socket.write(
+        [
+          'HTTP/1.1 200 Connection Established\r\n',
+          'Proxy-agent: jupiterdev-proxy\r\n',
+          '\r\n'
+        ].join('')
+      );
+      // Send head to target
+      targetSocket.write(head);
+      // Pipe data back and forth between client & target
+      targetSocket.pipe(socket);
+      socket.pipe(targetSocket);
+    });
+
+    // Error handlers
+    socket.on('error', onError);
+    targetSocket.on('error', onError);
+
+    // On close
+    socket.on('close', cleanup);
+    targetSocket.on('close', cleanup);
+  });
+
+  return require('http-shutdown')(httpServer);
+}
+
+const proxy = OProxy();
+
+function open() {
+  proxy.listen(8888, () => {
+    console.warn('OProxy listening ...');
+  });
+}
+
+function close () {
+  proxy.shutdown(() => {
+    console.log('Everything is cleanly shutdown.');
+  });
+}
+
+export {
+  open,
+  close
+}
