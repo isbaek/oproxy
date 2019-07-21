3e1f52f5e75d6fa679f7ddc58c4797e5f486845c
Inseo Baek <yunibaek@hotmail.com>
2019-07-21 13:14:41 +0200

    basic setup and get rid of unused internal assets

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,7 +1,5 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes';
 import styles from './Home.css';
 
 type Props = {};
@@ -11,9 +9,8 @@ export default class Home extends Component<Props> {
 
   render() {
     return (
-      <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
+      <div className={styles.container}>
+        <p>O Proxy</p>
       </div>
     );
   }
